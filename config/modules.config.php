5e5f949f5e89633c553f119d98051040e4b553e0
DuarteC <DuarteC@IT-WKS010.MidNet.CityOfMiddletown.com>
2020-04-22 09:13:20 -0400

    Initial Import

---
+++ config/modules.config.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-mvc-skeleton for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc-skeleton/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc-skeleton/blob/master/LICENSE.md New BSD License
+ */
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\I18n',
+    'Laminas\Db',
+    'Laminas\Navigation',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Application',
+    'Acl',
+    'Components',
+    'Settings',
+    'User',
+];
